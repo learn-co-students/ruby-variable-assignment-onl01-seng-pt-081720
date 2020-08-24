6db09f78c769953c6e6e16a5d19cd835b0efa765
Jessica Leising <jeleisin@gmail.com>
2020-08-24 20:17:47 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,22 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+authenticate_or_request_with_http_basic
+describe ""./variable.rb"
+./variable.rb = greeting
+./variable.rb
+greeting = "Hello World"
+
+end
+end
+
+
+
+
+"filter_parameter_logging"
+'filter_parameter_logging'
+authenticate_or_request_with_http_basic
+greeting = "Hello World"
+
+end 
+end
+
